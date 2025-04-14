36b868d3a14ecdfea3fdd6f295e1ec5ccf5b730c
hayaunderscore <stophayapls123@proton.me>
2025-04-14 11:38:06 +0800

    forgot the s here

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -142,7 +142,7 @@ return {
 		},
 		DescriptionDummy = {
 			dd_payasaka_adultcard_area = { name = "Sold Jokers", text = { "{C:inactive,s:0.8}Jokers listed have a chance of triggering{}" } },
-			dd_payasaka_property_card = { name = "Property", text = { "Each {C:green}house{} gives", "half the price of", "this {C:property}Property", "{C:inactive,0.8}Hotels are equivalent{}", "{C:inactive,0.8}to 5 houses{}" }}
+			dd_payasaka_property_card = { name = "Property", text = { "Each {C:green}house{} gives", "half the price of", "this {C:property}Property", "{C:inactive,s:0.8}Hotels are equivalent{}", "{C:inactive,s:0.8}to 5 houses{}" }}
 		},
 		Tarot = {
 			c_payasaka_greed = {
