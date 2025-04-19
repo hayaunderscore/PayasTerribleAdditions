19740328e9984b87a123a06db92c496a34977eb1
hayaunderscore <stophayapls123@proton.me>
2025-04-19 14:31:36 +0800

    remove hook on nether

--- content/blinds/nether.lua
+++ content/blinds/nether.lua
@@ -6,58 +6,55 @@ SMODS.Blind {
 	mult = 2,
 	boss = {min = 0, max = 1999},
 	boss_colour = HEX('35153F'),
-	set_blind = function (self)
+	set_blind = function(self)
 		play_sound("payasaka_nether")
 		G.GAME.payasaka_nether_destroycards = true
 	end,
-	disable = function (self)
+	disable = function(self)
 		G.GAME.payasaka_nether_destroycards = false
 	end,
-	defeat = function (self)
+	defeat = function(self)
 		G.GAME.payasaka_nether_destroycards = false
 	end,
-}
-
-local calc_context = SMODS.calculate_context
-function SMODS.calculate_context(context, return_table)
-	if G.GAME and G.GAME.payasaka_nether_destroycards and context.after then
-		local destroy = {}
-		for _, v in ipairs(G.play.cards) do
-			destroy[#destroy+1] = v
-		end
-		G.E_MANAGER:add_event(Event({
-			func = function()
-				for _, v in ipairs(destroy) do
-					if SMODS.shatters(v) then
-						v:shatter()
-					else
-						v:start_dissolve()
+	calculate = function(self, blind, context)
+		if context.after and G.GAME.payasaka_nether_destroycards then
+			local destroy = {}
+			for _, v in ipairs(G.play.cards) do
+				destroy[#destroy+1] = v
+			end
+			G.E_MANAGER:add_event(Event({
+				func = function()
+					for _, v in ipairs(destroy) do
+						if SMODS.shatters(v) then
+							v:shatter()
+						else
+							v:start_dissolve()
+						end
 					end
+
+					play_sound("payasaka_flint")
+
+					G.E_MANAGER:add_event(Event({
+						func = function()
+							SMODS.calculate_context({
+								remove_playing_cards = true,
+								removed = destroy
+							})
+							return true
+						end,
+					}))
+
+					return true
+				end,
+			}))
+
+			for _, v in ipairs(destroy) do
+				if SMODS.shatters(v) then
+					v.shattered = true
+				else
+					v.destroyed = true
 				end
-
-				play_sound("payasaka_flint")
-
-				G.E_MANAGER:add_event(Event({
-					func = function()
-						calc_context({
-							remove_playing_cards = true,
-							removed = destroy
-						})
-						return true
-					end,
-				}))
-
-				return true
-			end,
-		}))
-
-		for _, v in ipairs(destroy) do
-			if SMODS.shatters(v) then
-				v.shattered = true
-			else
-				v.destroyed = true
 			end
 		end
 	end
-	return calc_context(context, return_table)
-end
+}
