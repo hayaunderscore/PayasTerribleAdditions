f67157db9921af5e75d7749c781f0b92cf736d06
hayaunderscore <stophayapls123@proton.me>
2025-04-23 11:25:53 +0800

    tweaks to locomotive

--- content/jokers/ahead/shupo.lua
+++ content/jokers/ahead/shupo.lua
@@ -20,6 +20,7 @@ SMODS.Joker {
 	calculate = function(self, card, context)
 		if context.setting_blind then
 			local start_here = false
+			local played_sound = false
 			for j = 1, #G.jokers.cards do
 				if start_here and not G.jokers.cards[j].ability.eternal and G.jokers.cards[j].config.center.key ~= "j_payasaka_locomotive" then
 					if G.jokers.cards[j].ability.name ~= "cry-altgoogol" then
@@ -28,28 +29,31 @@ SMODS.Joker {
 					G.jokers.cards[j]:set_ability(G.P_CENTERS.j_payasaka_nil)
 					G.jokers.cards[j]:add_to_deck()
 					G.jokers.cards[j]:juice_up()
+					if not played_sound then
+						play_sound('slice1', 0.96 + math.random() * 0.08)
+						card:juice_up()
+						played_sound = true
+					end
 					card.ability.extra.x_chips = card.ability.extra.x_chips + card.ability.extra.give_x_chip
 					--card.states.drag.is = false
-					card_eval_status_text(G.jokers.cards[j], 'extra', nil, nil, nil, { message = "#(@(!--C##*}!", instant = true })
+					card_eval_status_text(G.jokers.cards[j], 'extra', nil, nil, nil, { message = "nil", instant = true })
 				end
 				if G.jokers.cards[j] == (card.area == PTASaka.adultcard_cardarea and PTASaka.adultcard_cardarea.pta_owner or card) then
 					start_here = true
 					-- pin it to the far right
-					G.E_MANAGER:add_event(Event{
+					G.E_MANAGER:add_event(Event {
 						func = function()
 							card.ability.pinned = true
 							card.pinned = true
 							card.old_sort_id = card.sort_id
 							card.sort_id = -9999999
-							play_sound('slice1', 0.96+math.random()*0.08)
-							card:juice_up()
 							return true
 						end
 					})
 				end
 			end
 			-- unpin it
-			G.E_MANAGER:add_event(Event{
+			G.E_MANAGER:add_event(Event {
 				trigger = "after",
 				func = function()
 					card.ability.pinned = false
