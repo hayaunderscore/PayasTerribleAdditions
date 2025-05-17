ad94fe48361c2dda735a6df5e6022433b32cc831
hayaunderscore <stophayapls123@proton.me>
2025-05-17 10:08:42 +0800

    improved marketipler

--- content/jokers/uncommon/xmult.lua
+++ content/jokers/uncommon/xmult.lua
@@ -30,11 +30,11 @@ end
 SMODS.Joker {
 	name = "Multiplier",
 	key = "markiplier_punch_gif",
-	config = { extra = { xmult_odds = 1, xmult_chance = 4, xchips_odds = 1, xchips_chance = 4 } },
+	config = { extra = { xmult_odds = 4, xchips_odds = 4 } },
 	loc_vars = function(self, info_queue, card)
 		local r = card.ability.cry_rigged
 		local oddmult = G.GAME.probabilities.normal or 1
-		return { vars = { r and card.ability.extra.xmult_chance or card.ability.extra.xmult_odds * oddmult, card.ability.extra.xmult_chance, r and card.ability.extra.xchips_chance or card.ability.extra.xchips_odds * oddmult, card.ability.extra.xchips_chance } }
+		return { vars = { r and card.ability.extra.xmult_odds or oddmult, card.ability.extra.xmult_odds, r and card.ability.extra.xchips_odds or oddmult, card.ability.extra.xchips_odds } }
 	end,
 	rarity = 2,
 	atlas = "JOE_Jokers",
@@ -48,35 +48,9 @@ SMODS.Joker {
 			colour = HEX('09d707')
 		},
 	},
-	update = function(self, card, dt)
-		if card.ability.cry_rigged then G.GAME.markiplier_prob_rigged = true end
-	end,
 	add_to_deck = function(self, card, from_debuff)
-		G.GAME.markiplier_prob_xmult_chance = G.GAME.markiplier_prob_xmult_chance + card.ability.extra.xmult_chance
-		G.GAME.markiplier_prob_xmult_odds = G.GAME.markiplier_prob_xmult_odds + card.ability.extra.xmult_odds
-		G.GAME.markiplier_prob_xchips_chance = G.GAME.markiplier_prob_xchips_chance + card.ability.extra.xchips_chance
-		G.GAME.markiplier_prob_xchips_odds = G.GAME.markiplier_prob_xchips_odds + card.ability.extra.xchips_odds
 		if not from_debuff then
 			play_sound("payasaka_markiplier_helloeverybody")
 		end
 	end,
-	remove_from_deck = function(self, card, from_debuff)
-		G.GAME.markiplier_prob_xmult_chance = G.GAME.markiplier_prob_xmult_chance - card.ability.extra.xmult_chance
-		G.GAME.markiplier_prob_xmult_odds = G.GAME.markiplier_prob_xmult_odds - card.ability.extra.xmult_odds
-		G.GAME.markiplier_prob_xchips_chance = G.GAME.markiplier_prob_xchips_chance - card.ability.extra.xchips_chance
-		G.GAME.markiplier_prob_xchips_odds = G.GAME.markiplier_prob_xchips_odds - card.ability.extra.xchips_odds
-		G.GAME.markiplier_prob_rigged = false
-	end
 }
-
-local igo = Game.init_game_object
-function Game:init_game_object()
-	local ret = igo(self)
-	-- Init multiplier related odds
-	ret.markiplier_prob_xmult_chance = 0
-	ret.markiplier_prob_xmult_odds = 0
-	ret.markiplier_prob_xchips_chance = 0
-	ret.markiplier_prob_xchips_odds = 0
-	ret.markiplier_prob_rigged = false
-	return ret
-end
