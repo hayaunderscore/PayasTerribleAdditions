527a3e7fea974d1d5d78a7a332f4e6fbb83d40c6
hayaunderscore <stophayapls123@proton.me>
2025-05-27 12:27:08 +0800

    reword some jokers and remove leftovers

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -82,18 +82,6 @@ return {
 					"{C:inactive,s:0.8}How absurd.",
 				}
 			},
-			j_payasaka_charred = {
-				name = "Charred Joker",
-				text = {
-					"Upgrade the level of",
-					"the first {C:attention}discarded",
-					"poker hand each round by {C:attention}#1#{}",
-					"{C:red}-1{} Discard",
-				},
-				payasaka_comment = {
-					"{C:inactive,s:0.8}Look at this joker...{}",
-				}
-			},
 			j_payasaka_newspaper = {
 				name = "Daily Newspaper",
 				text = {
@@ -116,21 +104,6 @@ return {
 				payasaka_comment = {
 					"{C:inactive,s:0.8}Does not affect other Photocopier{}",
 					"{C:inactive,s:0.8}If it did, that would be silly.{}"
-				}
-			},
-			j_payasaka_reistorm = {
-				name = "Reistorm",
-				text = {
-					"Retriggers all Jokers",
-					"an additional {C:attention}#1#{} times",
-					"with {C:green}#2# in #3#{} chance of",
-					"creating a random",
-					"{C:dark_edition}Negative{} {C:red}Rare{C:attention} Joker{}",
-					"at end of round",
-				},
-				payasaka_comment = {
-					"{C:inactive,s:0.8}Technically not a Brainstorm{}",
-					"{C:inactive,s:0.8}but whatever{}",
 				}
 			},
 			j_payasaka_goodnevil = {
@@ -352,12 +325,11 @@ return {
 			j_payasaka_locomotive = {
 				name = "Locomotive Getaway",
 				text = {
-					"Turn Jokers to the right",
-					"of this Joker to {C:red}nil{}",
-					"This Joker will {C:attention}always{} move",
-					"to the right of the list",
-					"{X:chips,C:white}X#1#{} Chips for every",
-					"Joker replaced",
+					"Selecting a blind moves this",
+					"Joker to the {C:attention}far-right{} and",
+					"{C:red}nil{}-ifies Jokers passed through",
+					"This Joker gains {X:chips,C:white}X#1#{} Chips for",
+					"every Joker {C:attention}replaced{} this way",
 					"{C:inactive}(Currently {X:chips,C:white}X#2#{C:inactive} Chips)",
 				},
 				payasaka_comment = {
@@ -416,11 +388,10 @@ return {
 			j_payasaka_btryacidspaghet = {
 				name = "Battery Acid Spaghetti",
 				text = {
-					"{X:mult,C:white}X#1#{} Blind Size",
-					"for {X:mult,C:white}X#2#{} Mult",
-					"Mult decreases by {X:mult,C:white}X#3#{}",
-					"at the {C:attention}end{} of round",
-					"{C:red}Self destructs",
+					"{X:mult,C:white}X#1#{} Blind Size for {X:mult,C:white}X#2#{} Mult",
+					"Mult decreases by {X:mult,C:white}X#3#{} at the",
+					"{C:attention}end{} of round and {C:red}self destructs",
+					"if XMult goes below {X:mult,C:white}X1"
 				},
 				payasaka_comment = {
 					"{C:inactive,s:0.8}don't do this"
