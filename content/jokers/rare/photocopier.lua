ac19d39a7aace7955136746a4f5091ece3b89388
BepisFever <nguyendoangiabao220808@gmail.com>
2025-05-31 21:44:54 +0700

    Update photocopier.lua

--- content/jokers/rare/photocopier.lua
+++ content/jokers/rare/photocopier.lua
@@ -20,7 +20,7 @@ end
 SMODS.Joker {
 	name = "Photocopier",
 	key = "photocopier",
-	config = { extra = { coolmult = 2 }, immutable = { old_ids = {}, payasaka_photocopied = {}, old_mult = 2 } },
+	config = { extra = { coolmult = 2, is_rightmost = false }, immutable = { old_ids = {}, payasaka_photocopied = {}, current_position = -1 } },
 	loc_vars = function(self, info_queue, card)
 		local mult = card.ability.extra.coolmult >= 1e300 and "a lot" or card.ability.extra.coolmult
 		return { vars = { mult } }
@@ -31,6 +31,10 @@ SMODS.Joker {
 	cost = 15,
 	blueprint_compat = false,
 	demicoloncompat = false,
+	add_to_deck = function(self, card, from_debuff)
+		card.ability.immutable.old_ids = {}
+		card.ability.immutable.payasaka_photocopied = {}
+	end,
 	update = function(self, card2, dt)
 		if G.STAGE == G.STAGES.RUN and card2.added_to_deck then
 			-- Get cards from left and right
@@ -43,6 +47,14 @@ SMODS.Joker {
 					end
 				end
 			end
+
+			local pos = -1
+			local area = card2.area
+			for i,v in ipairs(area and area.cards or {}) do
+				if v == card2 then pos = i break end
+			end
+			card2.ability.immutable.current_position = pos
+			card2.ability.extra.is_rightmost = not not (pos >= #G.jokers.cards)
 
 			-- do NOT exceed coolmult
 			card2.ability.extra.coolmult = math.min(card2.ability.extra.coolmult, 1e300)
@@ -57,24 +69,25 @@ SMODS.Joker {
 				cards[2].ability.immutable.payasaka_photocopied = cards[2].ability.immutable.payasaka_photocopied or {}
 			end
 
-			-- Do not affect other Photocopiers....
-			if cards[1] and cards[1].config.center.key == card2.config.center.key then cards[1] = nil end
-			if cards[2] and cards[2].config.center.key == card2.config.center.key then cards[2] = nil end
-
 			local mult = card2.ability.extra.coolmult
 			-- Switched jokers, halve the old one
-			if cards[1] and (card2.ability.immutable.old_ids[1] and card2.ability.immutable.old_ids[1] ~= cards[1].sort_id) then
+			if (not cards[1] and card2.ability.immutable.old_ids[1]) or (cards[1] and card2.ability.immutable.old_ids[1] and card2.ability.immutable.old_ids[1] ~= cards[1].sort_id) then
 				local jkr = find_joker_by_sort_id(card2.ability.immutable.old_ids[1])
 				if jkr and jkr.config and not Card.no(jkr, "immutable", true) then
 					PTASaka.Photocopy(jkr, mult, true, card2.sort_id)
 				end
 			end
-			if cards[2] and card2.ability.immutable.old_ids[2] and card2.ability.immutable.old_ids[2] ~= cards[2].sort_id then
+
+			if (not cards[2] and card2.ability.immutable.old_ids[2]) or (cards[2] and card2.ability.immutable.old_ids[2] and card2.ability.immutable.old_ids[2] ~= cards[2].sort_id) then
 				local jkr = find_joker_by_sort_id(card2.ability.immutable.old_ids[2])
 				if jkr and jkr.config and not Card.no(jkr, "immutable", true) then
 					PTASaka.Photocopy(jkr, mult, true, card2.sort_id)
 				end
 			end
+
+			-- Do not affect other Photocopiers....
+			if cards[1] and cards[1].config.center.key == card2.config.center.key then cards[1] = nil end
+			if cards[2] and cards[2].config.center.key == card2.config.center.key then cards[2] = nil end
 
 			-- Switched jokers, double the new one
 			if cards[1] and not cards[1].ability.immutable.payasaka_photocopied[card2.sort_id] and not Card.no(cards[1], "immutable", true) then
@@ -94,11 +107,15 @@ SMODS.Joker {
 		if G.STAGE ~= G.STAGES.RUN then return end
 		-- Get cards from left and right
 		local cards = { nil, nil }
-		for i = 1, #G.jokers.cards do
-			if G.jokers.cards[i] == card then
-				cards[1] = G.jokers.cards[i + 1]
-				if i > 1 then
-					cards[2] = G.jokers.cards[i - 1]
+		
+		if card.ability.immutable.current_position > 0 then
+			local first_pos = card.ability.immutable.current_position
+			if card.ability.extra.is_rightmost then
+				cards[2] = G.jokers.cards[#G.jokers.cards]
+			else
+				cards[1] = G.jokers.cards[first_pos]
+				if first_pos >= 2 then
+					cards[2] = G.jokers.cards[first_pos-1]
 				end
 			end
 		end
@@ -132,3 +149,4 @@ SMODS.Joker {
 		end
 	end
 }
+
