1d842959aacecf561991a45a5f5ab911a9a07b86
hayaunderscore <stophayapls123@proton.me>
2025-06-12 10:58:32 +0800

    aaaa

--- content/sleeve.lua
+++ content/sleeve.lua
@@ -6,13 +6,13 @@ CardSleeves.Sleeve {
 	pos = { x = 0, y = 0 },
 	apply = function(self, sleeve)
 		G.GAME.payasaka_allow_reroll = true
-		if self.get_current_deck_key() == "b_payasaka_gacha" or (G and G.GAME and G.GAME.payasaka_used_decks and G.GAME.payasaka_used_decks["b_payasaka_gacha"]) then
+		if self.get_current_deck_key() == "b_payasaka_gacha" or (G.STAGE == G.STAGES.RUN and G.GAME and G.GAME.payasaka_used_decks and G.GAME.payasaka_used_decks["b_payasaka_gacha"]) then
 			G.GAME.payasaka_unweighted_gacha = true
 		end
 	end,
 	loc_vars = function(self)
 		local key = self.key
-		if self.get_current_deck_key() == "b_payasaka_gacha" or (G and G.GAME and G.GAME.payasaka_used_decks and G.GAME.payasaka_used_decks["b_payasaka_gacha"]) then
+		if self.get_current_deck_key() == "b_payasaka_gacha" or (G.STAGE == G.STAGES.RUN and G.GAME and G.GAME.payasaka_used_decks and G.GAME.payasaka_used_decks["b_payasaka_gacha"]) then
 			key = self.key .. "_alt"
 		end
 		return { key = key }
