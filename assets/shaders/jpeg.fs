6901b904d1246905e9c06df9e39e5497fbba57fa
hayaunderscore <stophayapls123@proton.me>
2025-07-18 11:59:27 +0800

    fix the goddamn jpeg shader

--- assets/shaders/jpeg.fs
+++ assets/shaders/jpeg.fs
@@ -86,14 +86,17 @@ vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords
 
 	vec2 rndcoords = texture_coords;
 
-	float iTime = time;
+	float iTime = time+jpeg.r;
 	float t = time+jpeg.r;
+	
+	vec2 floored_uv = (floor((uv*texture_details.ba)))/max(texture_details.b, texture_details.a);
 
-	rndcoords.x += 0.0005 * (rand(vec2(iTime, uv.y)) - rand(vec2(iTime*6., uv.y*0.9)));
-	rndcoords.y += 0.001 * (rand(vec2(iTime*2., uv.y*0.9)) - rand(vec2(iTime*5., uv.y*0.8)));
-	//rndcoords.z += 0.012 * (rand(vec2(iTime*3., uv.y*0.8)) - rand(vec2(iTime*4., uv.y)));
+	rndcoords.x += 0.0005 * (rand(vec2(iTime, floored_uv.y)) - rand(vec2(iTime*6., floored_uv.y*0.9)));
+	rndcoords.y += 0.001 * (rand(vec2(iTime*2., floored_uv.y*0.9)) - rand(vec2(iTime*5., floored_uv.y*0.8)));
+	//rndcoords.z += 0.012 * (rand(vec2(iTime*3., floored_uv.y*0.8)) - rand(vec2(iTime*4., floored_uv.y)));
 
 	vec4 tex = Texel( texture, rndcoords );
+	tex = dissolve_mask(tex, rndcoords, uv);
 	vec3 col = tex.rgb;
 
 	// https://www.shadertoy.com/view/ttlGDf
@@ -109,8 +112,8 @@ vec4 effect( vec4 colour, Image texture, vec2 texture_coords, vec2 screen_coords
     col += 0.5 + 0.5*cos(iTime+uv.xyx+vec3(0.,2.,4.));
 	//col = pow(col, vec3(1.-0.4545));
 
-	tex = vec4(compress_ycbcr_844(col),tex.a*jpeg.r);
-	return dissolve_mask(tex, rndcoords, uv);
+	tex = vec4(compress_ycbcr_844(col),tex.a);
+	return tex;
 }
 
 extern MY_HIGHP_OR_MEDIUMP vec2 mouse_screen_pos;
