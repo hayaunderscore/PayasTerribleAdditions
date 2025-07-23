12fafb94d47ec86e1ae2577a15fcec5090418911
hayaunderscore <stophayapls123@proton.me>
2025-07-23 11:48:54 +0800

    fix huh all 20s

--- content/jokers/rare/ha20s.lua
+++ content/jokers/rare/ha20s.lua
@@ -1,3 +1,5 @@
+PTASaka.RetriggerableCenters = {}
+
 SMODS.Joker {
 	name = "Huh! All 20s",
 	key = "ha20s",
@@ -19,19 +21,18 @@ SMODS.Joker {
 	},
 	config = { extra = { trigger = false } },
 	calculate = function(self, card, context)
-		if ((context.retrigger_joker_check and not context.retrigger_joker) or context.repetition) and card.ability.extra.trigger then
-			return {
-				repetitions = 1,
-				func = function()
-					card.ability.extra.trigger = false
-				end
-			}
+		if ((context.retrigger_joker_check and not context.retrigger_joker) or context.repetition) then
+			if context.other_card and context.other_card.config and PTASaka.RetriggerableCenters[context.other_card.config.center_key] then
+				return {
+					repetitions = 1
+				}
+			end
 		end
-		if context.payasaka_pseudorandom_result and context.trigger_obj and not card.ability.extra.trigger then
+		if context.payasaka_pseudorandom_result and context.trigger_obj then
 			---@type Card
 			local c = context.trigger_obj
 			if type(c) == 'table' and c:is(Card) and c.ability and (c.ability.set == "Joker" or c.ability.set == "Enhanced" or c.ability.set == 'Default') then
-				card.ability.extra.trigger = true
+				PTASaka.RetriggerableCenters[c.config.center_key] = true
 			end
 		end
 	end,
