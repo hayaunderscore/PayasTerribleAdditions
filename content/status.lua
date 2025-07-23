76ebc399b32094e2b55c89eac8283a79095ed27a
hayaunderscore <stophayapls123@proton.me>
2025-07-23 11:18:24 +0800

    fixes again

--- content/status.lua
+++ content/status.lua
@@ -12,7 +12,7 @@ PTASaka.Status {
 				remove = true
 			}
 		end
-		if context.discard or (context.end_of_round and context.main_eval) then
+		if (context.discard and context.other_card == card) or (context.end_of_round and context.main_eval) then
 			self:apply(card, nil)
 		end
 	end,
@@ -36,7 +36,7 @@ PTASaka.Status {
 				target = G.hand
 			}
 		end
-		if context.discard or (context.end_of_round and context.main_eval) then
+		if (context.discard and context.other_card == card) or (context.end_of_round and context.main_eval) then
 			PTASaka.freeze_card(card, nil, true, true)
 		end
 	end,
