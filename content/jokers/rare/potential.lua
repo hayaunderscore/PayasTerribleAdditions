c373c1665dc8605cd07359971ea1c7ecc7f9268f
hayaunderscore <stophayapls123@proton.me>
2025-08-07 13:41:05 +0800

    add slight juicing to potential energy

--- content/jokers/rare/potential.lua
+++ content/jokers/rare/potential.lua
@@ -1,3 +1,35 @@
+function juice_card_endless(card, delay, id, remove, bypass)
+	local queue_name = "payasaka_potential_juice_"..tostring(id)
+	G.E_MANAGER.queues[queue_name] = G.E_MANAGER.queues[queue_name] or {}
+	if (not bypass) or (not card) then G.E_MANAGER:clear_queue(queue_name) end
+	if remove or (not card) then return end
+	G.E_MANAGER:add_event(Event({
+		trigger = 'after',
+		delay = delay or 0.1,
+		blocking = false,
+		blockable = false,
+		timer = 'REAL',
+		func = (function()
+			card:juice_up(0.1, 0.1)
+			juice_card_endless(card, 0.8, id, nil, true)
+			return true
+		end)
+	}), queue_name)
+end
+
+local p_h_ref = CardArea.parse_highlighted
+function CardArea:parse_highlighted()
+	p_h_ref(self)
+	local text = G.FUNCS.get_poker_hand_info(self.highlighted)
+	if self == G.hand and G.STATE == G.STATES.SELECTING_HAND then
+		for _, joker in ipairs(G.jokers.cards) do
+			if joker.ability.name == "pta-Potential Energy" then
+				juice_card_endless(joker, nil, joker.sort_id, joker.ability.extra.unique_hands[text or ""])
+			end
+		end
+	end
+end
+
 -- Potential Energy
 SMODS.Joker {
 	name = "pta-Potential Energy",
@@ -19,7 +51,7 @@ SMODS.Joker {
 			colour = HEX('09d707')
 		},
 	},
-	config = { extra = { num = 1, den = 10, unique_hands = {} } },
+	config = { extra = { num = 1, den = 10, unique_hands = { NULL = true } } },
 	calculate = function(self, card, context)
 		if context.before and not card.ability.extra.unique_hands[context.scoring_name] then
 			card.ability.extra.unique_hands[context.scoring_name] = true
@@ -28,12 +60,15 @@ SMODS.Joker {
 				message = localize('k_upgrade_ex')
 			}
 		end
+		if context.press_play then
+			juice_card_endless(card, nil, card.sort_id, true)
+		end
 		if context.selling_self and card.area and not context.blueprint_card then
 			if SMODS.pseudorandom_probability(card, 'potential', card.ability.extra.num, card.ability.extra.den) then
 				if #card.area.cards <= card.area.config.card_limit then
 					local eligible_copy = {}
 					for k, v in pairs(card.area.cards) do
-						if v and v ~= card then eligible_copy[#eligible_copy+1] = v end
+						if v and v ~= card then eligible_copy[#eligible_copy + 1] = v end
 					end
 					local copy = pseudorandom_element(eligible_copy, 'potential_jokr')
 					---@type Card
@@ -54,7 +89,7 @@ SMODS.Joker {
 			if #card.area.cards <= card.area.config.card_limit then
 				local eligible_copy = {}
 				for k, v in pairs(card.area.cards) do
-					if v and v ~= card then eligible_copy[#eligible_copy+1] = v end
+					if v and v ~= card then eligible_copy[#eligible_copy + 1] = v end
 				end
 				local copy = pseudorandom_element(eligible_copy, 'potential_jokr')
 				---@type Card
